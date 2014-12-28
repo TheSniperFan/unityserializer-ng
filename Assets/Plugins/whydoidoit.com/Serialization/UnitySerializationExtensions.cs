869a7b0236e4587334e3a86b65049142d23f428d
TheSniperFan <thesniperfan@gmail.com>
2014-12-28 17:17:25 +0100

    Just sorting
    Just sorting the serializers to group them in a more logical manner.

--- Assets/Plugins/whydoidoit.com/Serialization/UnitySerializationExtensions.cs
+++ Assets/Plugins/whydoidoit.com/Serialization/UnitySerializationExtensions.cs
@@ -6,82 +6,81 @@ using Serialization;
 using System.Linq;
 using System.Reflection;
 
-[Serializer(typeof(Vector3))]
-public class SerializeVector3 : SerializerExtensionBase<Vector3>
-{
-	public override IEnumerable<object> Save (Vector3 target)
-	{
-		return new object[] { target.x, target.y, target.z};
-	}
-	
-	public override object Load (object[] data, object instance)
-	{
+[Serializer(typeof(Vector2))]
+public class SerializeVector2 : SerializerExtensionBase<Vector2> {
+    public override IEnumerable<object> Save(Vector2 target) {
+        return new object[] { target.x, target.y };
+    }
+
+    public override object Load(object[] data, object instance) {
 #if US_LOGGING
 		Radical.Log ("Vector3: {0},{1},{2}", data [0], data [1], data [2]);
 #endif
-		return new UnitySerializer.DeferredSetter (d => {
-			if(!float.IsNaN((float)data[0]) && !float.IsNaN((float)data[1]) &&  !float.IsNaN((float)data[2]))
-				return new Vector3 ((float)data [0], (float)data [1], (float)data [2]);
-			else
-				return Vector3.zero;
-		}
-		);
-	}
+        return new Vector2((float)data[0], (float)data[1]);
+    }
 }
 
-[Serializer(typeof(Color))]
-public class SerializeColor : SerializerExtensionBase<Color>
-{
-	public override IEnumerable<object> Save (Color target)
-	{
-		return new object[] { target.r, target.g, target.b, target.a};
-	}
-	
-	public override object Load (object[] data, object instance)
-	{
+[Serializer(typeof(Vector3))]
+public class SerializeVector3 : SerializerExtensionBase<Vector3> {
+    public override IEnumerable<object> Save(Vector3 target) {
+        return new object[] { target.x, target.y, target.z };
+    }
+
+    public override object Load(object[] data, object instance) {
 #if US_LOGGING
 		Radical.Log ("Vector3: {0},{1},{2}", data [0], data [1], data [2]);
 #endif
-		return new Color((float)data[0],(float)data[1],(float)data[2],(float)data[3]);
-	}
+        return new UnitySerializer.DeferredSetter(d => {
+            if (!float.IsNaN((float)data[0]) && !float.IsNaN((float)data[1]) && !float.IsNaN((float)data[2]))
+                return new Vector3((float)data[0], (float)data[1], (float)data[2]);
+            else
+                return Vector3.zero;
+        }
+        );
+    }
 }
 
 [Serializer(typeof(Vector4))]
-public class SerializeVector4 : SerializerExtensionBase<Vector4>
-{
-	public override IEnumerable<object> Save (Vector4 target)
-	{
-		return new object[] { target.x, target.y, target.z, target.w};
-	}
-	
-	public override object Load (object[] data, object instance)
-	{
+public class SerializeVector4 : SerializerExtensionBase<Vector4> {
+    public override IEnumerable<object> Save(Vector4 target) {
+        return new object[] { target.x, target.y, target.z, target.w };
+    }
+
+    public override object Load(object[] data, object instance) {
 #if US_LOGGING
 		Radical.Log ("Vector3: {0},{1},{2}", data [0], data [1], data [2]);
 #endif
-			if(!float.IsNaN((float)data[0]) && !float.IsNaN((float)data[1]) &&  !float.IsNaN((float)data[2]) && !float.IsNaN((float)data[3]))
-				return new Vector4 ((float)data [0], (float)data [1], (float)data [2], (float)data[3]);
-			else
-				return Vector4.zero;
-	}
+        if (!float.IsNaN((float)data[0]) && !float.IsNaN((float)data[1]) && !float.IsNaN((float)data[2]) && !float.IsNaN((float)data[3]))
+            return new Vector4((float)data[0], (float)data[1], (float)data[2], (float)data[3]);
+        else
+            return Vector4.zero;
+    }
 }
 
-[Serializer(typeof(Vector2))]
-public class SerializeVector2 : SerializerExtensionBase<Vector2>
-{
-	public override IEnumerable<object> Save (Vector2 target)
-	{
-		return new object[] { target.x, target.y};
-	}
-	
-	public override object Load (object[] data, object instance)
-	{
+[Serializer(typeof(Quaternion))]
+public class SerializeQuaternion : SerializerExtensionBase<Quaternion> {
+    public override IEnumerable<object> Save(Quaternion target) {
+
+        return new object[] { target.x, target.y, target.z, target.w };
+    }
+
+    public override object Load(object[] data, object instance) {
+        return new UnitySerializer.DeferredSetter(d => new Quaternion((float)data[0], (float)data[1], (float)data[2], (float)data[3]));
+    }
+}
+
+[Serializer(typeof(Color))]
+public class SerializeColor : SerializerExtensionBase<Color> {
+    public override IEnumerable<object> Save(Color target) {
+        return new object[] { target.r, target.g, target.b, target.a };
+    }
+
+    public override object Load(object[] data, object instance) {
 #if US_LOGGING
 		Radical.Log ("Vector3: {0},{1},{2}", data [0], data [1], data [2]);
 #endif
-			return new Vector2 ((float)data [0], (float)data [1]);
-		
-	}
+        return new Color((float)data[0], (float)data[1], (float)data[2], (float)data[3]);
+    }
 }
 
 
@@ -132,21 +131,6 @@ public class SerializeWaitForSeconds : SerializerExtensionBase<WaitForSeconds>
 	public override object Load (object[] data, object instance)
 	{
 		return base.Load(data, instance);
-	}
-}
-
-[Serializer(typeof(Quaternion))]
-public class SerializeQuaternion : SerializerExtensionBase<Quaternion>
-{
-	public override IEnumerable<object> Save (Quaternion target)
-	{
-		
-		return new object[] { target.x, target.y, target.z, target.w};
-	}
-	
-	public override object Load (object[] data, object instance)
-	{
-		return new UnitySerializer.DeferredSetter (d => new Quaternion ((float)data [0], (float)data [1], (float)data [2], (float)data [3]));
 	}
 }
 
