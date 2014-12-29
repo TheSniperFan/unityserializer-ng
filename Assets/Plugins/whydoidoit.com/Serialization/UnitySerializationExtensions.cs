60fed7dac8f766649d0b40c7bce8baac52515c95
TheSniperFan <thesniperfan@gmail.com>
2014-12-29 14:44:11 +0100

    Extend BoxCollider support
    Store the PhysicsMaterial of the collider

--- Assets/Plugins/whydoidoit.com/Serialization/UnitySerializationExtensions.cs
+++ Assets/Plugins/whydoidoit.com/Serialization/UnitySerializationExtensions.cs
@@ -197,13 +197,14 @@ public class SerializerExtensionBase<T> : ISerializeObjectEx {
 [ComponentSerializerFor(typeof(BoxCollider))]
 public class SerializeBoxCollider : ComponentSerializerExtensionBase<BoxCollider> {
     public override IEnumerable<object> Save(BoxCollider target) {
-        return new object[] { target.isTrigger, target.size.x, target.size.y, target.size.z, target.center.x, target.center.y, target.center.z, target.enabled };
+        return new object[] { target.isTrigger, target.size.x, target.size.y, target.size.z, target.center.x, target.center.y, target.center.z, target.enabled, target.sharedMaterial };
     }
     public override void LoadInto(object[] data, BoxCollider instance) {
         instance.isTrigger = (bool)data[0];
         instance.size = new Vector3((float)data[1], (float)data[2], (float)data[3]);
         instance.center = new Vector3((float)data[4], (float)data[5], (float)data[6]);
         instance.enabled = (bool)data[7];
+        instance.sharedMaterial = (PhysicMaterial)data[8];
     }
 }
 
